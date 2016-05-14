a2e3ba7947464e0ffccf17ccd3f4e27f0d2e0e61
zhangshijie <tmyk104117@126.com>
2016-05-14 12:18:27 +0800

    add test html

--- __init__.py
+++ __init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask import make_response
+import time
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return render_template('index.html')
+	res = make_response(render_template('index.html'))
+	res.set_cookie(key='testcookie',value='haha',expires = time.time()+6*60)
+	return res
+@app.route("/test")
+def test():
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
